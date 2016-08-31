ebab5d613b6dabf4eb44cb0a9f74ab0c14dabe7a
Clare Moore <clare.moore@thinkabout.com.au>
2016-08-31 16:00:13 +1000

    Fix unit test folder

--- build.bat
+++ build.bat
@@ -25,7 +25,7 @@ REM call dotnet build --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
-cd src\Tests\Nugety.Tests
+cd src\Nugety.Tests
 call dotnet test  --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
