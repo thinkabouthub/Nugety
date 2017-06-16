b670e53fc74277204e872b54769f32cde2808c8b
Clare Moore <clare.moore@thinkabout.com.au>
2017-06-16 19:53:07 +1000

    Add extra output

--- src/Nugety.Project.Dependencies.DependencyCopy/Options.cs
+++ src/Nugety.Project.Dependencies.DependencyCopy/Options.cs
@@ -10,17 +10,17 @@ namespace Nugety.Project.Dependencies
     class Options
     {
 
-        [Option('s', "read", Required = true, HelpText = "Input files to be processed.")]
+        [Option('s', "read", Required = true, HelpText = "Source directory to be processed.")]
 
         public string Source { get; set; }
 
 
-        [Option('t', "read", Required = true,    HelpText = "Input files to be processed.")]
+        [Option('t', "read", Required = true,    HelpText = "Target directory to move dependency files to.")]
 
         public string Target { get; set; }
 
 
-        [Option('b', "read", Required = true,    HelpText = "Input files to be processed.")]
+        [Option('b', "read", Required = true,    HelpText = "Main app bin folder to check for dependencies already existing.")]
 
         public string MainAppBinFolder { get; set; }
     }
--- src/Nugety.Project.Dependencies.DependencyCopy/Program.cs
+++ src/Nugety.Project.Dependencies.DependencyCopy/Program.cs
@@ -21,12 +21,38 @@ namespace Nugety.Project.Dependencies
                 
                 if (Directory.Exists(options.Source) && Directory.Exists(options.MainAppBinFolder))
                 {
+                    Console.WriteLine("Target directory: " + options.Target);
+                    Console.WriteLine("Source directory: " + options.Source);
+                    Console.WriteLine("Main app bin directory: " + options.MainAppBinFolder);
+                    
                     DependencyUtil.ProcessDependencies(options.Source, options.Target, options.MainAppBinFolder);
+                }
+                else
+                {
+                    if (!Directory.Exists(options.Source))
+                    {
+                        Console.WriteLine("Source directory doesnt exist: " + options.Source);
+                    }
+                    if (!Directory.Exists(options.MainAppBinFolder))
+                    {
+                        Console.WriteLine("Main app bin directory doesnt exist: " + options.MainAppBinFolder);
+                    }
                 }
             }
             else
             {
                 Console.WriteLine("Must enter three folder arguments");
+
+                Console.WriteLine("Target directory: " + options.Target);
+                Console.WriteLine("Source directory: " + options.Source);
+                Console.WriteLine("Main app bin directory: " + options.MainAppBinFolder);
+
+                Console.WriteLine("args list" + args.Length);
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine("arg " + i + " value is " + args[i]);
+                }
             }
         }
     }
